277e3a024832b71b371f715332c1241cecd404e9
Valentin Wotschel <wotschel.valentin@googlemail.com>
2022-09-06 11:39:42 +0200

    Added tests #3

---
+++ tests/run-tests.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+
+# Stop on error
+set -e
+
+# Colors
+RED='\033[0;31m'
+BG_RED='\033[41m'
+BG_GREEN='\033[42m'
+NC='\033[0m'
+
+# Symbols
+CHECKMARK='\xE2\x9C\x94'
+CIRCLE='\xE2\x9A\xA0'
+
+cleanup_icons() {
+  # Remove all .ico files recursively
+  find . -name "*.ico" -type f -delete
+
+  # Remove dest directory
+  rm -rf dest
+}
+
+failed_tests=0
+
+fail_grep() {
+  test_name="$1"
+  output="$2"
+  expected_grep="$3"
+
+  echo -e "${BG_RED}Test failed${NC} - $test_name"
+  echo
+  echo -e "${RED}Expected output to contain:${NC}"
+  echo "$expected_grep"
+  echo
+  echo -e "${RED}Actual output:${NC}"
+  # Remove color codes
+  echo "$output" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g"
+  echo
+
+  failed_tests=$((failed_tests + 1))
+}
+
+fail_file() {
+  test_name="$1"
+  missing_file="$2"
+  expected_files="$3"
+  actual_files="$4"
+
+  echo -e "${BG_RED}Test failed${NC} - $test_name"
+  echo
+  echo -e "${RED}Expected output files:${NC}"
+  echo "$expected_files"
+  echo
+  echo -e "${RED}Actual output files:${NC}"
+  echo "$actual_files"
+  echo
+  echo -e "${RED}Missing file:${NC}"
+  echo "$missing_file"
+  echo
+
+  failed_tests=$((failed_tests + 1))
+}
+
+test_file() {
+  cleanup_icons
+
+  test_name="$1"
+  test_command="$2"
+  expected_files="$3"
+
+  # Run test
+  eval "$test_command" > /dev/null 2>&1
+
+  # Check if files exist
+  for file in $expected_files; do
+    if [ ! -f "$file" ]; then
+      # actual_files = all .ico files without without ./ prefix and in same line
+      actual_files=$(find . -name "*.ico" -type f | sed -r "s/^\.\///g" | tr '\n' ' ')
+      fail_file "$test_name" "$file" "$expected_files" "$actual_files"
+      return
+    fi
+  done
+
+  success "$test_name"
+}
+
+success() {
+  test_name="$1"
+
+  echo -e "${BG_GREEN}Test passed${NC} - $test_name"
+  echo
+}
+
+test_grep() {
+  cleanup_icons
+
+  # Disable errexit for this function
+  set +e
+
+  test_name="$1"
+  test_command="$2"
+  test_result="$(eval "$test_command" 2>&1)"
+  expected_grep="$3"
+
+  if [[ "$test_result" =~ $expected_grep ]]; then
+    success "$test_name"
+  else
+    fail_grep "$test_name" "$test_result" "$expected_grep"
+  fi
+}
+
+finally() {
+  cleanup_icons
+
+  # Print summary
+  if [ "$failed_tests" -gt 0 ]; then
+    echo -e "${RED}${CIRCLE} $failed_tests test(s) failed${NC}"
+    exit 1
+  else
+    echo -e "${CHECKMARK}  All tests passed"
+  fi
+}
+
+
+
+script_path="../svg-to-ico.sh"
+
+test_grep "Usage --help"     "'$script_path' --help"                 "Usage:"
+test_grep "Usage -h"         "'$script_path' -h"                     "Usage:"
+test_grep "Usage no args"    "'$script_path'"                        "Usage:"
+test_grep "Invalid file"     "'$script_path' invalid-file.svg"       "Input is not a valid file or directory"
+test_grep "Invalid dir"      "'$script_path' invalid-dir"            "Input is not a valid file or directory"
+test_grep "Invalid output"   "'$script_path' folder im-a-file.ico"   "Output must be a directory when input is a directory"
+test_grep "Invalid padding"  "'$script_path' folder -p im-a-string"  "Padding option must be a number"
+test_grep "Invalid tasks"    "'$script_path' folder -t im-a-string"  "Tasks option must be a number"
+test_grep "Invalid flag"     "'$script_path' folder -x"              "Unsupported flag"
+test_grep "Invalid option"   "'$script_path' folder --invalid"       "Unsupported option"
+
+test_file "Single file"         "'$script_path' test.svg"           "test.ico"
+test_file "Single file out"     "'$script_path' test.svg dest.ico"  "dest.ico"
+test_file "Single file in dir"  "'$script_path' folder/test.svg"    "folder/test.ico"
+
+test_file "Directory"         "'$script_path' folder"                 "folder/subfolder/test.ico folder/test.ico"
+test_file "Subdirectory"      "'$script_path' folder/subfolder"       "folder/subfolder/test.ico"
+test_file "Directory out"     "'$script_path' folder dest"            "dest/subfolder/test.ico dest/test.ico"
+test_file "Subdirectory out"  "'$script_path' folder/subfolder dest"  "dest/test.ico"
+
+test_file "Directory padding"   "'$script_path' folder -p 10"  "folder/subfolder/test.ico folder/test.ico"
+test_file "Directory tasks"     "'$script_path' folder -t 1"   "folder/subfolder/test.ico folder/test.ico"
+test_file "Directory parallel"  "'$script_path' folder -t 0"   "folder/subfolder/test.ico folder/test.ico" 1
+
+finally
